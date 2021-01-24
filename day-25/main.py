0bfe7b0d42d819d4ce0d2dcac0460a0a32574535
Mark Lee <lmk302@gmail.com>
2021-01-24 11:39:44 -0800

    nato_phonetic_alphabet

--- day-25/main.py
+++ day-25/main.py
@@ -1,1 +1,80 @@
-# 
+# # with open("weather_data.csv") as data_file:
+# #     data = data_file.readlines()
+# #     print(data)
+
+# # import csv
+
+# # with open("weather_Data.csv") as data_file:
+# #     data = csv.reader(data_file)
+# #     temperature = []
+# #     for row in data:
+# #         if row[1] != "temp":
+# #             temperature.append(int(row[1]))
+# #     print(temperature)
+
+# import pandas
+
+# data = pandas.read_csv("weather_Data.csv")
+# # print(type(data))
+# # print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+
+# # sum_temp = 0
+# # for temp in temp_list:
+# #     sum_temp += temp
+# # ave_temp = sum_temp / len(temp_list)
+# # print (ave_temp)
+
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+
+# #Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# #Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+# #Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+# print(data)
+
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["grey","Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
--- day-26/main.py
+++ day-26/main.py
@@ -1,1 +1,81 @@
-# 
+
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+
+new_list.append(add_1)
+
+#List comprehension
+
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
+
+name = "Angela"
+new_list = [letter for letter in name]
+print(new_list)
+
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+result = [number for number in numbers if number % 2 == 0]
+print(result)
+
+with open ("file1.txt") as file1:
+    file1_data = file1.readlines()
+    
+with open ("file2.txt") as file2:
+    file2_data = file2.readlines()
+    
+file12 = [int(number) for number in file1_data if number in file2_data]
+print(file12)
+
+#dictionary comprehension
+
+import random
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+students_scores = {student:random.randint(1, 100) for student in names}
+print(students_scores)
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 80}
+print(passed_students)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word:len(word) for word in sentence.split()}
+
+print(result)
+
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+print(weather_c)
+print(weather_f)
+
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+for (key, value) in student_dict.items():
+    print(value)
+
+import pandas
+student_df = pandas.DataFrame(student_dict)
+print(student_df)
+
+# for (key, value) in student_df.items():
+#     print(value)
+
+#Loop through rows of a data frame
+for (index, row) in student_df.iterrows():
+    print(row.student)
+
+
