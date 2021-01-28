9dff9b3041c3582c43459b95d9e9df3b2d3f5f0b
Mark Lee <lmk302@gmail.com>
2021-01-27 16:54:15 -0800

    pomodoro

--- day-27/main.py
+++ day-27/main.py
@@ -1,1 +1,16 @@
-# 
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+
+#Label
+
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24))
+my_label.pack(side="left")
+
+
+
+
+window.mainloop()
