a546958ac9caced22287caf5d0089913810c0977
Mark Lee <lmk302@gmail.com>
2021-01-31 21:44:38 -0800

    iss notification

--- day-32/main.py
+++ day-32/main.py
@@ -1,1 +1,25 @@
-# 
+# import smtplib
+
+# my_email = "appbreweryinfo@gmail.com"
+# password = "abcd1234()"
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="appbrewerytesting@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
+
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15)
+print(date_of_birth)
--- day-33/main.py
+++ day-33/main.py
@@ -1,1 +1,53 @@
-# 
+# from tkinter import *
+# import requests
+
+
+# def get_quote():
+#     response = requests.get("https://api.kanye.rest")
+#     response.raise_for_status()
+#     data = response.json()
+#     quote = data["quote"]
+#     canvas.itemconfig(quote_text, text=quote)
+
+
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
+
+# canvas = Canvas(width=300, height=414)
+# background_img = PhotoImage(file="background.png")
+# canvas.create_image(150, 207, image=background_img)
+# quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
+# canvas.grid(row=0, column=0)
+
+# kanye_img = PhotoImage(file="kanye.png")
+# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+# kanye_button.grid(row=1, column=0)
+
+
+
+# window.mainloop()
+
+
+
+import requests
+from datetime import datetime
+
+MY_LAT = 50.507351
+MY_LONG = -0.127758
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
